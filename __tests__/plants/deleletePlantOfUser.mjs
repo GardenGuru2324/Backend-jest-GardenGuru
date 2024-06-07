e3d56891cb2fa9bbd547685414dd44822ac507b3
MilanMareels <milan.mareels@gmail.com>
2024-06-07 13:54:01 +0200

    added some checks to see of hte plant is deleted

--- __tests__/plants/deleletePlantOfUser.mjs
+++ __tests__/plants/deleletePlantOfUser.mjs
@@ -1,73 +1,84 @@
-import { describe, it, expect } from '@jest/globals';
-import * as client from '../../src/clients/clients.mjs';
-import { initializeDatabase } from '../../src/database/initializeDatabase.mjs';
-import { clearDatabase } from '../../src/database/clearDatabase.mjs';
-import { succesMessages } from '../../src/messages/messages.mjs';
-import { errorMessages } from '../../src/errors/errors.mjs';
+import { describe, it, expect } from "@jest/globals";
+import * as client from "../../src/clients/clients.mjs";
+import { initializeDatabase } from "../../src/database/initializeDatabase.mjs";
+import { clearDatabase } from "../../src/database/clearDatabase.mjs";
+import { succesMessages } from "../../src/messages/messages.mjs";
+import { errorMessages } from "../../src/errors/errors.mjs";
+
+const objectForChecksPlantToBeDeleted = [
+	{
+		it: `Plant with plantId jest_plant_1_user_1 should be deleted`,
+		plantId: "jest_plant_1_user_1"
+	},
+	{
+		it: "Plant with plantId jest_plant_3_user_2 should be deleted",
+		plantId: "jest_plant_3_user_2"
+	}
+];
 
 const objectMessages = [
 	{
-		it: 'delete plant of user and return correct message',
-		userId: 'jest_user_1',
-		plantId: 'jest_plant_1_user_1',
+		it: "delete plant with plantId jest_plant_1_user_1 of user and return correct message",
+		userId: "jest_user_1",
+		plantId: "jest_plant_1_user_1",
 		expect: succesMessages.SuccesfullyDeletePlant
 	},
 	{
-		it: 'not delete plant of user when user does not exists and return correct message',
-		userId: '12345',
-		plantId: 'jest_plant_1_user_1',
+		it: "not delete plant of user when user does not exists and return correct message",
+		userId: "12345",
+		plantId: "jest_plant_1_user_1",
 		expect: errorMessages.userNotFound
 	},
 	{
-		it: 'not delete plant when plant does not exists and return correct message',
-		userId: 'jest_user_1',
-		plantId: '12345',
+		it: "not delete plant when plant does not exists and return correct message",
+		userId: "jest_user_1",
+		plantId: "12345",
 		expect: errorMessages.plantNotFound
 	},
 	{
-		it: 'not delete plant when user is not owner of the plant and return correct message',
-		userId: 'jest_user_2',
-		plantId: 'jest_plant_1_user_1',
+		it: "not delete plant when user is not owner of the plant and return correct message",
+		userId: "jest_user_2",
+		plantId: "jest_plant_1_user_1",
 		expect: errorMessages.plantNotFound
 	}
 ];
 
 const objectStatusCode = [
 	{
-		it: 'delete plant of user and return correct statusCode',
-		userId: 'jest_user_2',
-		plantId: 'jest_plant_3_user_2',
+		it: "delete plant with plantId jest_plant_3_user_2 of user and return correct statusCode",
+		userId: "jest_user_2",
+		plantId: "jest_plant_3_user_2",
 		expect: 200
 	},
 	{
-		it: 'not delete plant of user when user does not exists and return correct statusCode',
-		userId: '12345',
-		plantId: 'jest_plant_1_user_1',
+		it: "not delete plant of user when user does not exists and return correct statusCode",
+		userId: "12345",
+		plantId: "jest_plant_1_user_1",
 		expect: 404
 	},
 	{
-		it: 'not delete plant when plant does not exists and return correct statusCode',
-		userId: 'jest_user_1',
-		plantId: '12345',
+		it: "not delete plant when plant does not exists and return correct statusCode",
+		userId: "jest_user_1",
+		plantId: "12345",
 		expect: 404
 	},
 	{
-		it: 'not delete plant when user is not owner of the plant and return correct statusCode',
-		userId: 'jest_user_2',
-		plantId: 'jest_plant_1_user_1',
+		it: "not delete plant when user is not owner of the plant and return correct statusCode",
+		userId: "jest_user_2",
+		plantId: "jest_plant_1_user_1",
 		expect: 404
 	}
 ];
 
-describe('Delete plant of user', () => {
+describe("Delete plant of user", () => {
 	beforeAll(async () => {
-		await initializeDatabase('Plants');
-		await initializeDatabase('Users');
+		await initializeDatabase("Plants");
+		await initializeDatabase("Users");
 	});
 
 	afterAll(async () => {
-		await clearDatabase('Plants');
-		await clearDatabase('Users');
+		await clearDatabase("Plants");
+		await clearDatabase("Users");
 	});
 
 	objectMessages.forEach((obj) => {
@@ -85,6 +96,17 @@ describe('Delete plant of user', () => {
 			const result = await client.deletePlantOfUser(obj.userId, obj.plantId);
 
 			expect(result.statusCode).toEqual(obj.expect);
+		});
+	});
+
+	objectForChecksPlantToBeDeleted.forEach((obj) => {
+		it(`${obj.it}`, async () => {
+			const result = await client.getAllPlants();
+
+			const expectedResult = JSON.parse(result.text);
+			const expectedPlant = expectedResult.find((plant) => plant.plantId === obj.plantId);
+
+			expect(expectedPlant).toBeUndefined();
 		});
 	});
 });
--- src/clients/clients.mjs
+++ src/clients/clients.mjs
@@ -1,11 +1,15 @@
-import * as superagent from 'superagent';
-import 'dotenv/config';
+import * as superagent from "superagent";
+import "dotenv/config";
 
 const agent = superagent.agent();
 const baseUrl = process.env.BASE_URL;
 
 export const getApi = async () => {
 	return await agent.get(baseUrl);
+};
+
+export const getAllPlants = async () => {
+	return await agent.get(`${baseUrl}/plants`);
 };
 
 export const deletePlantOfUser = async (userId, plantId) => {
