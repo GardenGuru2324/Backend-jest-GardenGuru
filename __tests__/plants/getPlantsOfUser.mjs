5f38a508a4403732bd9981a75f05ac50fb6040b5
Renzo-Declerck <declerck.renzo@outlook.com>
2024-06-10 16:02:39 +0200

    feat: adding test-cases for status codes of fetching the plants of an user

--- __tests__/plants/getPlantsOfUser.mjs
+++ __tests__/plants/getPlantsOfUser.mjs
@@ -2,7 +2,10 @@ import { describe, it, expect } from "@jest/globals";
 import * as client from "../../src/clients/clients.mjs";
 import { initializeDatabase } from "../../src/database/initializeDatabase.mjs";
 import { clearDatabase } from "../../src/database/clearDatabase.mjs";
-import { objectMessages } from "../../src/lib/deletePlant/getPlantsObjects.mjs";
+import {
+  objectStatusCodes,
+  objectMessages,
+} from "../../src/lib/deletePlant/getPlantsObjects.mjs";
 
 describe("Get plants of user", () => {
   beforeAll(async () => {
@@ -15,10 +18,17 @@ describe("Get plants of user", () => {
     await clearDatabase("Users");
   });
 
+  objectStatusCodes.forEach((obj) => {
+    it(`Should ${obj.it}`, async () => {
+      const result = await client.getAllPlantsOfUser(obj.userId);
+
+      expect(result.statusCode).toEqual(obj.expect);
+    });
+  });
+
   objectMessages.forEach((obj) => {
     it(`Should ${obj.it}`, async () => {
       const response = await client.getAllPlantsOfUser(obj.userId);
-
       const result = JSON.parse(response.text);
 
       expect(result.message).toEqual(obj.expect);
--- src/lib/getPlants/getPlantsObjects.mjs
+++ src/lib/getPlants/getPlantsObjects.mjs
@@ -1,6 +1,24 @@
 import { errorMessages } from "../../errors/errors.mjs";
 import { succesMessages } from "../../messages/messages.mjs";
 
+export const objectStatusCodes = [
+    {
+        it: "get plants of user and return correct status code",
+        userId: "jest_user_1",
+        expect: 200,
+    },
+    {
+        it: "not get any plant of user with no plants and return correct status code",
+        userId: "jest_user_3",
+        expect: 400,
+    },
+    {
+        it: "not get any plant of user who does not exist and return correct status code",
+        userId: "jest_user_0",
+        expect: 409,
+    }
+]
+
 export const objectMessages = [
   {
     it: "get plants of user and return correct message",
