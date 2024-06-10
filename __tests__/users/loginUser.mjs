018126bff6d84925bb3bd064b173b7e6f0292860
MilanMareels <milan.mareels@gmail.com>
2024-06-10 19:07:41 +0200

    test one done

--- __tests__/users/loginUser.mjs
+++ __tests__/users/loginUser.mjs
@@ -2,6 +2,7 @@ import { describe, it, expect } from "@jest/globals";
 import * as client from "../../src/clients/clients.mjs";
 import { initializeDatabase } from "../../src/database/initializeDatabase.mjs";
 import { clearDatabase } from "../../src/database/clearDatabase.mjs";
+import { objectMessages } from "../../src/lib/loginUser/loginUserObjects.mjs";
 
 describe("Login user", () => {
 	beforeAll(async () => {
@@ -12,9 +13,34 @@ describe("Login user", () => {
 		await clearDatabase("Users");
 	});
 
+	it(`Should login an user with email jest_user_1@example.com`, async () => {
+		const user = {
+			email: "jest_user_1@example.com",
+			password: "admin123"
+		};
+
+		const expectedUser = {
+			_id: "666730eb1cca1f0d6338ec65",
+			userId: "jest_user_1",
+			userName: "jest_user_1",
+			password: "$2a$12$N.7.wzAYOuSFIJdTetqNhOFJpRRyfUnCS/qzAUK2nhDjne2uF7YtS",
+			ratingPlaced: true,
+			profilePicture: "https://example.com/profile/johndoe.jpg",
+			email: "jest_user_1@example.com",
+			fullName: "jest_user_1",
+			accountCreated: 1602960000
+		};
+
+		const result = await client.loginUser(user);
+
+		const expectedResult = JSON.parse(result.text);
+
+		expect(expectedResult).toEqual(expectedUser);
+	});
+
 	objectMessages.forEach((obj) => {
 		it(`Should ${obj.it}`, async () => {
-			const result = await client.deletePlantOfUser(obj.userId, obj.plantId);
+			const result = await client.loginUser(obj.user);
 
 			const expectedResult = JSON.parse(result.text);
 
--- src/lib/loginUser/loginUserObjects.mjs
+++ src/lib/loginUser/loginUserObjects.mjs
@@ -1,14 +1,35 @@
 import { errorMessages } from "../../errors/errors.mjs";
 import { succesMessages } from "../../messages/messages.mjs";
 
-export const objectMessages = [
+export const objectLoginSuccesUser = [
 	{
 		it: "login an user with email jest_user_1@example.com",
 		user: {
-            email: "jest_user_1@example.com",
-            password: "admin123"
-        },
-		expect: succesMessages.SuccesfullyDeletePlant
+			email: "jest_user_1@example.com",
+			password: "admin123"
+		},
+		expect: {
+			_id: "666730eb1cca1f0d6338ec65",
+			userId: "jest_user_1",
+			userName: "jest_user_1",
+			password: "$2a$12$N.7.wzAYOuSFIJdTetqNhOFJpRRyfUnCS/qzAUK2nhDjne2uF7YtS",
+			ratingPlaced: true,
+			profilePicture: "https://example.com/profile/johndoe.jpg",
+			email: "jest_user_1@example.com",
+			fullName: "jest_user_1",
+			accountCreated: 1602960000
+		}
+	}
+];
+
+export const objectMessages = [
+	{
+		it: "an login an user when user does not exist",
+		user: {
+			email: "jest_user_nonExistingUser@example.com",
+			password: "admin123"
+		},
+		expect: errorMessages.userNotFound
 	}
 ];
 
