7c73cbb6f388fe4c56b9bd284608d35c9f651701
MilanMareels <milan.mareels@gmail.com>
2024-06-11 14:27:15 +0200

    fixed seeding

--- src/database/initializeDatabase.mjs
+++ src/database/initializeDatabase.mjs
@@ -1,9 +1,9 @@
-import * as superagent from 'superagent';
-import 'dotenv/config';
+import * as superagent from "superagent";
+import "dotenv/config";
 
 const agent = superagent.agent();
 const baseUrl = process.env.BASE_URL;
 
 export const initializeDatabase = async (tableName) => {
-	return await agent.get(`${baseUrl}/${tableName}/initializeDatabase`);
+	return await agent.get(`${baseUrl}/initializeDatabase/${tableName}`);
 };
