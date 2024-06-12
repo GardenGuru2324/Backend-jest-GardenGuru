eb331936b30487e571fdbe2d96cc1ad5a57ebad5
Renzo-Declerck <declerck.renzo@outlook.com>
2024-06-12 11:28:05 +0200

    feat: adding test-cases for response messages of fetching a plant

--- __tests__/plants/getPlant.mjs
+++ __tests__/plants/getPlant.mjs
@@ -2,7 +2,10 @@ import { describe, it, expect } from "@jest/globals";
 import * as client from "../../src/clients/clients.mjs";
 import { initializeDatabase } from "../../src/database/initializeDatabase.mjs";
 import { clearDatabase } from "../../src/database/clearDatabase.mjs";
-import { objectStatusCodes } from "../../src/lib/getPlants/getPlantObjects.mjs";
+import {
+  objectStatusCodes,
+  objectMessages,
+} from "../../src/lib/getPlants/getPlantObjects.mjs";
 
 describe("Fetches the information of a single plant", () => {
   beforeAll(async () => {
@@ -18,6 +21,15 @@ describe("Fetches the information of a single plant", () => {
       const result = await client.getPlant(obj.plantId);
 
       expect(result.statusCode).toEqual(obj.expect);
+    });
+  });
+
+  objectMessages.forEach((obj) => {
+    it(`Should ${obj.it}`, async () => {
+      const response = await client.getPlant(obj.plantId);
+      const result = JSON.parse(response.text);
+
+      expect(result.message).toEqual(obj.expect);
     });
   });
 });
--- src/lib/getPlants/getPlantObjects.mjs
+++ src/lib/getPlants/getPlantObjects.mjs
@@ -1,3 +1,5 @@
+import { errorMessages } from "../../errors/errors.mjs";
+
 export const objectStatusCodes = [
   {
     it: "get plant and return correct status code",
@@ -6,7 +8,15 @@ export const objectStatusCodes = [
   },
   {
     it: "not get plant and return correct status code",
-    userId: "jest_plant_0_user_0",
+    plantId: "jest_plant_0_user_0",
     expect: 404,
+  },
+];
+
+export const objectMessages = [
+  {
+    it: "not get plant and return correct error message",
+    plantId: "jest_plant_0_user_0",
+    expect: errorMessages.plantNotFound,
   },
 ];
