24b7ed56e96de1008e0c3ec8dcd35faee69c455e
Renzo-Declerck <declerck.renzo@outlook.com>
2024-07-17 10:09:51 +0200

    feat: adding test for the statusCode when adding a plant

---
+++ src/lib/addPlant/addPlantObjects.mjs
@@ -0,0 +1,23 @@
+const randomNumber = Math.floor(Math.random() * 1000);
+
+export const objectStatusCode = {
+  it: "succesfully add a plant and return correct status code",
+  newPlant: {
+    plantId: `jest_plant_${randomNumber}_user_1`,
+    plantName: "New Plant Based On 1_1",
+    locationId: "jest_plantLocation_1",
+    plantImage: "https://dummyFoto.jpg",
+    plantedDate: 1602960000,
+    userId: "jest_user_1",
+    isVegetable: false,
+    plantGrowthHabit: "Climbing",
+    plantAvgHeight: 1,
+    plantMaxHeight: 2,
+    plantGrowthRate: "Fast",
+    plantDaysToHarvest: 0,
+    plantRowSpacing: 0,
+    plantMinTemp: 18,
+    plantMaxTemp: 30,
+  },
+  expect: 201,
+};
