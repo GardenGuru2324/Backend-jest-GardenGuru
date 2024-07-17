c751678dbd62a02b391af16d4145aab7b5d856a0
Renzo-Declerck <declerck.renzo@outlook.com>
2024-07-17 10:10:30 +0200

    feat: adding test for the message when adding a plant

--- __tests__/plants/addPlant.mjs
+++ __tests__/plants/addPlant.mjs
@@ -1,1 +1,33 @@
+import { describe, it, expect } from "@jest/globals";
+import * as client from "../../src/clients/clients.mjs";
+import { initializeDatabase } from "../../src/database/initializeDatabase.mjs";
+import { clearDatabase } from "../../src/database/clearDatabase.mjs";
+import {
+  objectStatusCode,
+  objectMessage,
+} from "../../src/lib/addPlant/addPlantObjects.mjs";
+
+describe("Add a plant", () => {
+  beforeAll(async () => {
+    await initializeDatabase("Users");
+    await initializeDatabase("Plants");
+  });
+
+  afterAll(async () => {
+    await clearDatabase("Users");
+    await clearDatabase("Plants");
+  });
+
+  it.skip(`Should ${objectStatusCode.it}`, async () => {
+    const result = await client.registerUser(objectStatusCode.newPlant);
+
+    expect(result.statusCode).toEqual(objectStatusCode.expect);
+  });
+
+  it.skip(`Should ${objectMessage.it}`, async () => {
+    const result = await client.registerUser(objectMessage.newPlant);
+    const expectedResult = JSON.parse(result.text);
+
+    expect(expectedResult.message).toEqual(objectMessage.expect);
+  });
 });
--- src/lib/addPlant/addPlantObjects.mjs
+++ src/lib/addPlant/addPlantObjects.mjs
@@ -1,3 +1,5 @@
+import { succesMessages } from "../../messages/messages.mjs";
+
 const randomNumber = Math.floor(Math.random() * 1000);
 
 export const objectStatusCode = {
@@ -20,4 +22,26 @@ export const objectStatusCode = {
     plantMaxTemp: 30,
   },
   expect: 201,
+};
+
+export const objectMessage = {
+  it: "succesfully add a plant and return correct status message",
+  newPlant: {
+    plantId: `jest_plant_${randomNumber}_user_1`,
+    plantName: "New Plant Based On 1_1",
+    locationId: "jest_plantLocation_1",
+    plantImage: "https://dummyFoto.jpg",
+    plantedDate: 1602960000,
+    userId: "jest_user_1",
+    isVegetable: false,
+    plantGrowthHabit: "Climbing",
+    plantAvgHeight: 1,
+    plantMaxHeight: 2,
+    plantGrowthRate: "Fast",
+    plantDaysToHarvest: 0,
+    plantRowSpacing: 0,
+    plantMinTemp: 18,
+    plantMaxTemp: 30,
+  },
+  expect: succesMessages.succesfullyAddPlant,
 };
