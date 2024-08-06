69c66ec1fdffecd6a83ec04cdded5fbf1cd6fda4
Renzo-Declerck <137225593+Renzo-Declerck@users.noreply.github.com>
2024-08-06 13:05:52 +0200

    Test/guru 43 (#13)
    * feat: adding endpoint-call for updating profile picture
    
    * feat: adding test for checking status code when updating profile picture
    
    * feat: adding test for checking object when updating profile picture

---
+++ src/lib/updateProfilePicture/updateProfilePictureObjects.mjs
@@ -0,0 +1,6 @@
+export const objectStatusCodes = {
+    it: "succesfully update the profile picture and return correct status code",
+    userId: "jest_user_1",
+    newProfilePicture: "https://picsum.photos/200",
+    expect: 200,
+}
